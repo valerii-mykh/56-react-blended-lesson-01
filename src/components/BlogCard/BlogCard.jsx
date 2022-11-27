06dfc1417de7fa313cb739efc6d657f3c56fadd0
Valerii Mykhailenko <58910653+valerii-mykh@users.noreply.github.com>
2022-11-27 11:27:04 +0200

    add

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -12,8 +12,35 @@ import {
   UserInfo,
   UserName,
   Date,
-} from './BlogCard.styled';
+} from "./BlogCard.styled";
 
 export const BlogCard = () => {
-  return <div>BlogCard</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster
+          src="https://source.unsplash.com/600x400/?computer"
+          alt="card__image"
+        />
+      </CardHeader>
+      <CardBody>
+        <Tag>Technology</Tag>
+        <CardTitle>What's new in 2022 Tech</CardTitle>
+        <CardText>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea
+          atque quidem!
+        </CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src="https://i.pravatar.cc/40?img=1" alt="Jane Doe" />
+          <UserInfo>
+            <UserName>Jane Doe</UserName>
+            <Date>2h ago</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
 };
